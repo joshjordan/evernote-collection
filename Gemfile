59237e23530e0788e3b732415accc962362d9e5a
Josh Jordan <josh.jordan@gmail.com>
2014-10-28 16:23:55 -0400

    Resize and cache 'full' and thumbnail images

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra'
 
 gem 'dalli'
 gem 'evernote_oauth'
+gem 'fastimage_resize'
 gem 'hashie'
 
 group :development do
--- app.rb
+++ app.rb
@@ -8,7 +8,31 @@ get '/' do
 end
 
 get %r{/items/(.*)} do |resource_guid|
-  resource = NoteStore.new.resource(resource_guid)
-  content_type resource.mime
-  resource.data.body
+  data = settings.cache.get("resource:#{resource_guid}")
+  if !data
+    resource = NoteStore.new.resource(resource_guid)
+    resized = FastImage.resize(StringIO.new(resource.data.body), 0, 400)
+    data = File.read(resized)
+    settings.cache.set("resource:#{resource_guid}", data)
+  end
+
+  content_type 'image/jpeg'
+  data
+end
+
+get %r{/thumbs/(.*)} do |resource_guid|
+  data = settings.cache.get("resource-thumb:#{resource_guid}")
+  if !data
+    resource = NoteStore.new.resource(resource_guid)
+    resized = FastImage.resize(StringIO.new(resource.data.body), 125, 0)
+    data = File.read(resized)
+    settings.cache.set("resource-thumb:#{resource_guid}", data)
+  end
+
+  content_type 'image/jpeg'
+  data
+end
+
+get '/console' do
+  binding.pry
 end
